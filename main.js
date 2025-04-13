0ec609da112936ff1fd7c8ed2175da5837db7393
ehsan <ehsanahmadi8338@gmail.com>
2025-04-13 14:51:55 +0330

    handle sum

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-const form = document.querySelector('.form_numbers');
-const numberInputs = form.querySelectorAll('input[type="number"]');
-const textInput = form.querySelector('input[type="text"]');
+const numberInputs = document.querySelectorAll('.my_number_value input[type="number"]');
+const textInput = document.querySelector('.my_number_total');
 
 numberInputs.forEach(input => {
   input.addEventListener('input', () => {
     const sum = Array.from(numberInputs)
       .reduce((total, input) => total + (Number(input.value) || 0), 0);
 
-    textInput.value = sum;
+    textInput.innerText = `Computed Twig ${sum}`;
   });
 });
